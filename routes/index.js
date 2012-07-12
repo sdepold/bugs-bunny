0a63e833cfa878dadb1647f132ed131afd13e476
sdepold <sascha@depold.com>
2012-07-12 09:17:33 +0200

    show additional notifications

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var helpers = require(__dirname + '/../lib/helpers')
 
 exports.index = function(req, res) {
   res.render('index', {
-    title:            'Express',
+    title:            'Bugs Bunny',
     mailAddresses:    helpers.getMailAddresses(),
+    notifications:    helpers.getNotificationAddresses(),
     currentBugsBunny: helpers.getCurrentMailData()
   })
 }
