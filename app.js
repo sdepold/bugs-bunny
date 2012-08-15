a183ade7db38c30aec8b0f277b8180ff7056e2bb
sdepold <sascha@depold.com>
2012-08-15 09:42:02 +0200

    added button for resending the mails

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.post('/', function(req, res) {
+  helpers.sendMails()
+  res.redirect('/')
+})
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
